9a7a4b602f12fe383724e11c832766340dea4370
Cody Romano <codyromano@gmail.com>
2017-12-22 22:59:51 -0500

    Sort articles alphabetically

--- routes/getTableOfContents.js
+++ routes/getTableOfContents.js
@@ -2,7 +2,7 @@ const getSQLQueryResult = require('../database/getSQLQueryResult');
 
 module.exports = function getTableOfContents(app) {
   app.get('/table-of-contents/', (req, res) => {
-    const sql = `SELECT id,title FROM articles`;
+    const sql = `SELECT id,title FROM articles ORDER BY title`;
 
     getSQLQueryResult(sql, result => {
       res.send({
