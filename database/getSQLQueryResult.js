302d3738add37a4b5f19d21bfd6532401673d30e
Cody Romano <codyromano@gmail.com>
2017-12-26 11:07:30 -0500

    Avoid MySQL connection timeout

--- database/getSQLQueryResult.js
+++ database/getSQLQueryResult.js
@@ -1,16 +1,20 @@
 const mysql = require('mysql');
 const config = require('./config');
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: config.user,
-  password: config.password,
-  database: config.database
-});
-
 module.exports = function getSQLQueryResult(sql, callback) {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: config.user,
+    password: config.password,
+    database: config.database
+  });
+
+  connection.connect();
+
   connection.query(sql, function (err, result) {
     if (err) throw err;
     callback(result);
   });
+
+  connection.end();
 };
